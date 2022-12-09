1456a11b18d3ef5876ccca4edd7aa17e1a81b5b5
Guilherme <guilhermepytech1408@gmail.com>
2022-12-09 18:16:30 -0300

    Terminando o CRUD

--- src/slugfy.py
+++
@@ -1,8 +0,0 @@
-
-def slugfy(nome: str) -> str:
-    """
-       Uma função que recebe uma string qualquer e retorna uma versão dessa string segura para endereços da web.
-       Acentos são removidos, espaços são convertidos em hifens.
-       Exemplo: "Toda proparoxítona tem acento" => "toda-proparoxitona-tem-acento"
-    """
-    raise NotImplementedError("Não implementada")

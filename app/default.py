34fe988be1693ab0f72f37d15b8fe74fb833119e
Gabriely-Silva <gabrielyfreitas696@gmail.com>
2022-12-11 21:09:41 -0300

    Banco de dados terminado

--- app/default.py
+++ app/default.py
@@ -1,12 +1,12 @@
 from App import app, db
 from flask import render_template, request, redirect, url_for
-from App.tables import User
+from App.tables import User, Disciplina
 
 
 @app.route("/users")
 def index():
     usuarios = db.session.execute(db.select(User).order_by(User.username)).scalars()
-    return render_template("tabela_usuarios.html", users=usuarios)
+    return render_template("usuarios.html", users=usuarios)
     
 
 @app.route("/users/add", methods=["GET", "POST"])
@@ -19,5 +19,19 @@ def add():
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for("tabela_usuarios"))
+        return redirect(url_for("index"))
     return render_template("cadastro.html")
+
+@app.route("/home", methods=["GET", "POST"])
+def cadastra():
+    if request.method == "POST":
+        disciplina = Disciplina(
+            nome=request.form["disciplina"], 
+            ch=request.form["ch"],
+            periodo=request.form["periodo"],
+            media=request.form["media"])
+            
+        db.session.add(disciplina)
+        db.session.commit()
+
+    return render_template("disciplinas.html")
--- app/tables.py
+++ app/tables.py
@@ -4,11 +4,34 @@ from App import app, db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String)
-    email = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
 
     def __repr__(self):
         return User(username= self.username, email=self.email)
 
+class Disciplina(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String)
+    
+    def __repr__(self):
+        return Disciplina(nome= self.nome)
+
+class User_Disci(db.Model):
+   id_userdisci = db.Column(db.Integer, primary_key=True, autoincrement=True)
+   id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+   User = db.relationship('User', foreign_keys=id_user)
+   email_user = db.Column(db.String, db.ForeignKey('user.email'))
+   id_disciplina = db.Column(db.Integer, db.ForeignKey('disciplina.id'))
+   Disciplina = db.relationship('Disciplina', foreign_keys=id_disciplina)
+   nome_disci = db.Column(db.String, db.ForeignKey('disciplina.nome'))
+   ch = db.Column(db.Integer)
+   periodo = db.Column(db.Integer)
+   media = db.Column(db.Float)
+   
+   def __repr__(self):
+       return User_Disci(id_user= self.id_user, email_user=self.email_user, id_disciplina = self.id_disciplina,
+            nome_disci=self.nome_disci,ch= self.ch, periodo= self.periodo, media = self.media)
+
 db.init_app(app)
-"""with app.app_context():
-    db.create_all()"""
+with app.app_context():
+    db.create_all()
